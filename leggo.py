6c3669a69033ba8cc8ca41178fc4cd9fbdceb972
mirkofabbrizi22-collab <mirkofabbrizi22@gmail.com>
2026-02-02 09:41:22 +0100

    Add files via upload

---
+++ leggo.py
@@ -0,0 +1,9 @@
+nomefile = "logfile.txt"
+
+try:
+    f = open(nomefile, "r", encoding='utf-8')
+    righe = f.readlines()
+    for riga in righe:
+        print(riga, end='')
+except FileNotFoundError as e:
+    print (f"[-] errore bloccante: {str(e)}")
